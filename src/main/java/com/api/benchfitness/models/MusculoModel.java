6c9e2362b3cfb4281aaf61c609f801143ce32b10
OscarLuque-31 <a_oscar.luque.hidalgo@iespablopicasso.es>
2024-12-25 18:04:07 +0100

    arreglo base de datos

--- src/main/java/com/api/benchfitness/models/MusculoModel.java
+++ src/main/java/com/api/benchfitness/models/MusculoModel.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "musculos")
+@Table(name = "Musculos")
 public class MusculoModel {
 	
 	@Id
--- src/main/java/com/api/benchfitness/models/NivelModel.java
+++ src/main/java/com/api/benchfitness/models/NivelModel.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "niveles")
+@Table(name = "Niveles")
 public class NivelModel {
 
 	@Id
