5c3757d60edc93e0d33d3894460173837b35a92f
OscarLuque-31 <oscar11luquexd@gmail.com>
2025-04-04 10:04:21 +0200

    prueba api key 2

--- src/main/java/com/api/benchfitness/security/SecurityConfig.java
+++ src/main/java/com/api/benchfitness/security/SecurityConfig.java
@@ -5,20 +5,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
 	@Value("${security.api.key}")
 	private String apiKey;
-	
+
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		
+
 		ApiKeyAuthFilter apiKeyFilter = new ApiKeyAuthFilter(apiKey);
 
-        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth.requestMatchers("/public/**").permitAll().anyRequest().authenticated()).addFilterBefore(apiKeyFilter, org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class);
-	
+		 http.csrf(csrf -> csrf.disable()) // Desactiva CSRF si no es necesario
+	        .authorizeHttpRequests(auth -> auth
+	            .anyRequest().permitAll()) // Permite todas las solicitudes para depurar
+	        .addFilterBefore(apiKeyFilter, UsernamePasswordAuthenticationFilter.class); // Añadimos el filtro de API Key
+
 		return http.build();
 	}
 }
